80c07d5da2391b1ef3388299c0e356e6b0e10d50
Edwin Kamande <kamande254@gmail.com>
2024-05-24 00:42:32 +0300

    Final working version

--- php/processMatrix.php
+++ php/processMatrix.php
@@ -7,35 +7,47 @@
  * 3.the number of columns of the matrix that contain repeated elements.
 
 */
-function processMatrix($arr) {
+class ProcessMatrix {
+    private $arr;
+    private $size;
 
-    $size = count($arr[$i]);
 
-    function diagonoalSum() {
+    public function __construct($arr, $size) {
+        $this->arr = $arr;
+        $this->size = $size;
+    }
+
+    public function diagonoalSum() {
         $sum = 0;
 
-        for ($i = 0; $i < $size; $i++) {
-            $sum += $arr[$i][$i];
+        for ($i = 0; $i < $this->size; $i++) {
+            $sum += $this->arr[$i][$i];
         }
         return $sum;
     } //    end diagonalSum
 
-    function rowCount() {
+    public function rowCount() {
         /** return number of rows with duplicated elements in a matrix */
+
         $count = 0;
-        $visited = array(); //Associative array to store encountered values
+        $visited = array(); //Associative array to store encountered values 
 
         // For each element in row checks if it exists in visited associative array
-         for ($i = 0; $i < $size; $i++) {
-            for ($j = 0; $j < $size; $j++) {
+         for ($i = 0; $i < $this->size; $i++) {
+            
+            $visited = array(); // empty to track next row
+           
+            for ($j = 0; $j < $this->size; $j++) {
 
-                $value = $arr[$i][$j];
-                if (isset($visited[$value])) {
-                    echo "The row contains duplicate values.";
-                    $count += 1;
-                    continue; // Exit loop after finding a duplicate
+                $value = $this->arr[$i][$j];
+                
+                if (!isset($visited[$value])) {
+                    $visited[$value] = 1;
                   }
-                  $visited[$value] = true;
+                else{
+                     $count += 1;
+                     break;
+                  }
                 }
             }
 
@@ -43,40 +55,48 @@ function processMatrix($arr) {
          }
     
 
-    function columnCount() {
+    public function columnCount() {
         /** return number of COLUMNS with duplicated elements in a matrix */
 
         $count = 0;
         $visited = array(); //Associative array to store encountered values
 
         // For each element in column checks if it exists in visited associative array
-         for ($i = 0; $i < $size; $i++) {
-            for ($j = 0; $j < $size; $j++) {
+         for ($i = 0; $i < $this->size; $i++) {
 
-                $value = $arr[$j][$i];
-                if (isset($visited[$value])) {
+            $visited = array(); // empty to track next column
 
-                    echo "The row contains duplicate values.";
-                    $count += 1;
-                    continue; // Exit loop after finding a duplicate
+            for ($j = 0; $j < $this->size; $j++) {
+
+                $value = $this->arr[$j][$i];
+
+                if (!isset($visited[$value])) {
+                    $visited[$value] = 1;
                   }
-                  $visited[$value] = true;
+                else{
+                    //  if similar element alread traversed , increment count and break to next column
+                    $count += 1;
+                    break;
+                }
                 }
             }
 
          return $count;
         }
+    
+        public function jsonData() {
+            $diagnaolSum = $this->diagonoalSum(); 
+            $rowCount = $this->rowCount(); 
+            $columnCount = $this->columnCount();
+    
+            $data = array($diagnaolSum, $rowCount, $columnCount);
+    
+            $json_data = json_encode($data);
+            
+            return $json_data;
+        }
 
-        $diagnaolSum = diagonoalSum(); 
-        $rowCount = rowCount(); 
-        $columnCount = columnCount();
-
-        $data = array($diagnaolSum, $rowCount, $columnCount);
-
-        $json_data = json_encode($data);
-        
-        return $json_data;
-    }
+        }
 
     $matrix = array(
         0 => array(1, 2, 3, 4),
@@ -84,8 +104,23 @@ function processMatrix($arr) {
         2 => array(3, 4, 1, 2),
         3 => array(4, 3, 2, 1)
         );
+    
+    $size = count($matrix);
 
-    $processMatrix = processMatrix($matrix);
-    echo $processMatrix;
+    $processMatrix = new ProcessMatrix($matrix, $size);
+    echo $processMatrix->jsonData();
+
+    $matrix = array(
+        0 => array(2, 2, 2, 2),
+        1 => array(2, 3, 2, 3),
+        2 => array(2, 2, 2, 3),
+        3 => array(2, 2, 2, 2)
+        );
+    
+    $size = count($matrix);
+
+    $processMatrix = new ProcessMatrix($matrix, $size);
+    echo $processMatrix->jsonData();
+ 
  
 ?>
