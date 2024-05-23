fadcf6919c5ae7164c6eebb3ce3e6454bfc56a78
Edwin Kamande <kamande254@gmail.com>
2024-05-23 18:58:00 +0300

    update use function

--- php/ReverString.php
+++ php/ReverString.php
@@ -1,10 +1,9 @@
 <?php
-echo "Hello World";
     /**
      * Trim spaces at start and end of sentence
      * Split the sentence into array of words separated
      */
-class ReversString {
+// class ReversString {
     // @param string $sentence
     //  return string
 
@@ -20,9 +19,9 @@ class ReversString {
         return implode(' ', $reversedWords);
 
     }
-}
+// }
 
-$reverseInstance = new ReversString();
-$answer = $reverseInstance->reverseString(" no changes added to commit");
+// $reverseInstance = new ReversString();
+$answer = reverseString(" no changes added to commit");
 echo $answer;
 ?>
